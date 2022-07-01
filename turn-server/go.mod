1b53b11b70725f4a6b7b626399205e291319b3fc
cryshado <mirmiclub@gmail.com>
2022-07-01 18:20:20 +0300

    turn-server implementation

---
+++ turn-server/go.mod
@@ -0,0 +1,17 @@
+module turn-server
+
+go 1.18
+
+require (
+	github.com/joho/godotenv v1.4.0
+	github.com/pion/turn/v2 v2.0.8
+	github.com/sirupsen/logrus v1.8.1
+)
+
+require (
+	github.com/pion/logging v0.2.2 // indirect
+	github.com/pion/randutil v0.1.0 // indirect
+	github.com/pion/stun v0.3.5 // indirect
+	github.com/pion/transport v0.13.0 // indirect
+	golang.org/x/sys v0.0.0-20220319134239-a9b59b0215f8 // indirect
+)
---
+++ turn-server/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+	"turn-server/config"
+	_ "turn-server/log"
+
+	"github.com/pion/turn/v2"
+	"github.com/sirupsen/logrus"
+)
+
+func authHandler(user string, realm string, srcAddr net.Addr) ([]byte, bool) {
+	// always allow access
+	key := turn.GenerateAuthKey(
+		"default-user",
+		config.CFG.Realm,
+		"default-password",
+	)
+
+	return key, true
+}
+
+func main() {
+	logrus.Info("Starting the \"turn-server\"")
+
+	config.Configure()
+
+	hostPort := fmt.Sprintf("%s:%s", config.CFG.Host, strconv.Itoa(config.CFG.Port))
+	logrus.Infof("public-ip:	%s", config.CFG.PublicIP)
+	logrus.Infof("host:port:	%s", hostPort)
+
+	udpLstnr, err := net.ListenPacket("udp4", hostPort)
+
+	if err != nil {
+		logrus.Fatalf("Error while creating TURN server UDP listener: %s", err)
+	}
+
+	server, err := turn.NewServer(turn.ServerConfig{
+		Realm:       config.CFG.Realm,
+		AuthHandler: authHandler,
+		PacketConnConfigs: []turn.PacketConnConfig{
+			{
+				PacketConn: udpLstnr,
+				RelayAddressGenerator: &turn.RelayAddressGeneratorStatic{
+					RelayAddress: net.ParseIP(config.CFG.PublicIP),
+					Address:      config.CFG.Host,
+				},
+			},
+		},
+	})
+
+	if err != nil {
+		logrus.Fatalf("Error while turn.NewServer: %s", err)
+	}
+
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+	<-signals
+
+	if err = server.Close(); err != nil {
+		logrus.Fatalf("Error while server.Close(): %s", err)
+	}
+}
